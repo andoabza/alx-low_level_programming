1e974ef34f5592c82f107b6bdca94aaa44f3c88f
ando <terbu102@gmail.com>
2023-03-28 11:55:10 -0700

    sum-all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,7 +3,7 @@
 /**
  * sum_them_all - calculates the sum of all its parameters
  * @n: number of arguments passed to the function
- *
+ 
  * Return: the resulting sum
  */
 int sum_them_all(const unsigned int n, ...)
