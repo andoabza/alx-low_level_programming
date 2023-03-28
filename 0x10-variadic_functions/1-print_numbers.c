f257eedfd7933349cf0740bafb47c47fac2c4778
ando <terbu102@gmail.com>
2023-03-28 12:12:33 -0700

    correction numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -4,6 +4,7 @@
  * print_numbers - prints numbers given as parameters
  * @separator: string to be printed between numbers
  * @n: number of integers passed to the function
+ *
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
