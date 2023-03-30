5491679540ca67a3a1b4a02a73e2f8b078f329d8
ando <terbu102@gmail.com>
2023-03-30 06:40:35 -0700

    print

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,7 +4,6 @@
 /**
  * print_list - prints all the elements of a linked list
  * @h: pointer to the list_t list to print
- *
  * Return: the number of nodes printed
  */
 size_t print_list(const list_t *h)
