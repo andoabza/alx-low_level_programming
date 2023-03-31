e95bfc9b1dfd211f064b3fded897aa2c343be9ab
ando <terbu102@gmail.com>
2023-03-31 00:03:06 -0700

    100 first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,7 +3,6 @@
 void first(void) __attribute__ ((constructor));
 
 /**
- *
  * first - prints a sentence before the main
  * function is executed
  */
@@ -11,4 +10,8 @@ void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
+}
+int main(void)
+{
+  return 0;
 }
