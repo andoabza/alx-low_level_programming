56906ea596285c44c6d379d0722ccdfa86b1d6ae
ando <terbu102@gmail.com>
2023-03-31 22:07:50 -0700

    fibonaci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
- * followed by a new line
+ * always  followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
