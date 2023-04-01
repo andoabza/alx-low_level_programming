397ddbee8ce7d3be36985a9b69edf3249fa00ad2
ando <terbu102@gmail.com>
2023-03-31 22:11:28 -0700

    to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,7 +3,7 @@
 
 /**
  * print_to_98 - prints all natural numbers from n to 98,
- * followed by a new line
+ * always followed by a new line
  * @n: print from this number
  */
 void print_to_98(int n)
