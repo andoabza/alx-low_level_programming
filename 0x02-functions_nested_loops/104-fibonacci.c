d725f7f25ac9e2200370371b495e3678d8597d4f
ando <terbu102@gmail.com>
2023-03-31 22:09:19 -0700

    104 fibona

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -3,7 +3,7 @@
 /**
  * main - finds and prints the first 98 Fibonacci numbers,
  * starting with 1 and 2
- * followed by a new line
+ * always followed by a new line
  * Return: ALways 0 (Success)
  */
 int main(void)
