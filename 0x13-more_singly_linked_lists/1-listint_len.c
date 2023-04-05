0612dfec325a2c8dd83a80e40996d9386e143d84
ando abza <terbu102@gmail.com>
2023-04-05 11:13:17 -0700

    test

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -7,33 +7,16 @@
  * Return: number of nodes
  */
 
-/**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * 
- */
-
-typedef struct listint_t
-{
-  int n;
-
-  struct listint_t *next;
-
-} listint_t;  
-
   
 size_t listint_len(const listint_t *h)
 {
-	size_t num = 0;
+  size_t num = 0;
 
-	while (h)
-	{
-		num++;
-		h = h->next;
-	}
+  while (h)
+    {
+      num++;
+      h = h->next;
+    }
 
-	return (num);
+  return (num);
 }
