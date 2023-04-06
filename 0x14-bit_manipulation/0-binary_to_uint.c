7a10b4d9108c3da70a49ddb859b19a44859ea5f8
ando abza <terbu102@gmail.com>
2023-04-06 09:32:53 -0700

    te

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,6 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
+
 /**
  * binary_to_uint - convert binary number to int
  * @b: char string
@@ -8,24 +9,24 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-  unsigned int nums, times;
-  int len;
-
-  if (b == NULL)
-    return (0);
-
-  for (len = 0; b[len]; len++)
-    {
-      if (b[len] != '\0' && b[len] != '1')
-	return (0);
-    }
-  
-  for (times = 1, nums = 0, len--; len >= 0; len--, times *= 2)
-    {
-      if (b[len] == '1')
-	nums += times;
-    }
-  
-  return (nums);
+        unsigned int nums, times;
+	int len;
+	
+	if (b == NULL)
+	        return (0);
+	
+	for (len = 0; b[len]; len++)
+	{
+	        if (b[len] != '\0' && b[len] != '1')
+		        return (0);
+	}
+	
+	for (times = 1, nums = 0, len--; len >= 0; len--, times *= 2)
+	{
+	        if (b[len] == '1')
+		        nums += times;
+	}
+	
+	return (nums);
 }
   
