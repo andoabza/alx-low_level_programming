d1c9198f19969a9baa8acb624faa000cf16d3c01
ando abza <terbu102@gmail.com>
2023-04-10 10:38:52 -0700

    tes

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,20 +10,18 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-  int o, w, len = 0;
-
-  if (filename == NULL)
+  int err, len, fd;
+  err = len = fd = 0;
+  if (!filename)
     return (-1);
-
-  if (text_content != NULL)
-    {
-      for (len = 0; text_content[len];)
-	len++;
-    }
-  o = open(filename, O_CREAT | O_RWDR | O_TRUNC, 0600);
-  w = write(o, text_content, len);
-  if (o == -1 || w == -1)
-    return (1);
-  close(o);
+  fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+  if (fd < 0)
+    return (-1);
+  while (text_content && text_content[len])
+    len++;
+  err = write(fd, text_content, len);
+  if (err < 0)
+    return (-1);
+  close(fd);
   return (1);
 }
