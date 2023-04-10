6d0009ee5a36375f0b5a76c8e81948d5670a2d1b
ando abza <terbu102@gmail.com>
2023-04-10 12:58:30 -0700

    gg

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,4 +1,6 @@
-#include "main.h"m
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 #define BUFSIZE 1024
 
 static ssize_t read_file(char *file, char **buf, int fd);
@@ -12,5 +14,27 @@ static void write_copy(char *file, int fd, char *buf, int len);
  */
 int main(int ac, char *av[])
 {
-        int fd_0, fd_1, rd_len, err;
-        char *buf, *file_from, *file_to;
+  int fd_0, fd_1, rd_len, err;
+  char *buf, *file_from, *file_to;
+
+  buff = NULL;
+  rd_len = 1;
+  if (ac != 3)
+    {
+      dprintf(STD_ERR_FILENO, "usage: cp file_from file_to\n");
+      exit(97);
+    }
+  file_from = av[1];
+  file_to = av[2];
+  fd_0 = open(file_from, O_RDONLY);
+  fd_1 = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664)
+    while (rd_len > 0)
+      {
+       /* Read the content from origin file */
+       rd_len = read_file(file_from, &buf, fd_0);
+       if (!rd_len)
+	 break;
+       /* Write out the buffer to destination */
+       write_copy(file_to, fd_1, buf, rd_len);
+      }
+}
