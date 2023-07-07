fb7ecf087b7d560edffa8be9d3041e52a01480c4
ando <terbu102@gmail.com>
2023-07-07 09:52:28 -0700

    str

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+/*
+ *  *str to be append
+ * */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+    unsigned long int hash = 5381;
+    int c;
+
+    while ((c = *str++)) 
+    {
+        hash = ((hash << 5) + hash) + c; // hash * 33 + c
+    }
+
+    return (hash);
+}
