909e3567988d1a8a460e5265e4bc611acb92c212
ando <terbu102@gmail.com>
2023-07-07 10:11:05 -0700

    implem

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -9,7 +9,7 @@ unsigned long int hash_djb2(const unsigned char *str)
 
     while ((c = *str++)) 
     {
-        hash = ((hash << 5) + hash) + c; // hash * 33 + c
+        hash = ((hash << 5) + hash) + c;
     }
 
     return (hash);
