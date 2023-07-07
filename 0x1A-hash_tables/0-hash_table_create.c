a5053df372b42daab241caf600949bfe619e1906
ando <terbu102@gmail.com>
2023-07-07 09:37:28 -0700

    add

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,30 @@
+#include "hash_table.h"
+/*
+ * size is the size of the array
+ *
+ **/
+ hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_table = NULL;
+    	unsigned long int i;
+
+	hash_table = malloc(sizeof(hash_table_t));
+    	if (hash_table == NULL) {
+        	return NULL;
+   	 }
+
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
+    	if (hash_table->array == NULL) {
+        	free(hash_table);
+        	return NULL;
+    	}
+
+    	for (i = 0; i < size; i++) {
+        	hash_table->array[i] = NULL;
+    	}
+
+    	hash_table->size = size;
+
+    	return hash_table;
+}
+
